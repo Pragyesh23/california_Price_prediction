b680ae20558a6b82df2d167addae409c445d636a
vedgya <66718791+vedgya@users.noreply.github.com>
2022-03-02 23:34:29 +0530

    fix l1

--- pragyesh.py
+++ pragyesh.py
@@ -1,7 +1,3 @@
 print("Hello I am Pragyesh")
 print("Hello Pragyesh, I am Vedgya")
-<<<<<<< HEAD
-l1=[1,2,3,4,5]
-=======
-l1=[1,2,3,4]
->>>>>>> 44a2334bef088ea48efe5cd2634c3e9054c07442
+l1 = [1,2,3,4]
